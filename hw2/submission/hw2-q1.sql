0f7d1825eca8f1ccafef637d29246de2a073a37d
byorxyz <ohmyheroine@hotmail.com>
2017-10-17 00:10:40 -0700

    q1 done

--- hw2/submission/hw2-q1.sql
+++ hw2/submission/hw2-q1.sql
@@ -1,5 +1,7 @@
 /*
-(10 points) List the distinct flight numbers of all flights from Seattle to Boston by Alaska Airlines Inc. on Mondays. Also notice that, in the database, the city names include the state. So Seattle appears as Seattle WA.
+(10 points) List the distinct flight numbers of all flights from Seattle 
+to Boston by Alaska Airlines Inc. on Mondays. 
+
 Name the output column flight_num.
 [Hint: Output relation cardinality: 3 rows]
 
@@ -16,7 +18,15 @@ create table CARRIERS (cid varchar(7) PRIMARY KEY, name varchar(83));
 create table MONTHS (mid int PRIMARY KEY, month varchar(9));
 
 create table WEEKDAYS (did int PRIMARY KEY, day_of_week varchar(9));
-
-
 */
 
+.header on
+.mode column
+.nullvalue NULL
+
+select DISTINCT F.flight_num as flight_num 
+from FLIGHTS as F, CARRIERS as C, WEEKDAYS as W
+where F.origin_city = "Seattle WA"
+and F.dest_city = "Boston MA"
+and C.name = "Alaska Airlines Inc."
+and W.day_of_week = "Monday";
