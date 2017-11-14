f68aabeb4da755b4307bffdf511222f5971482da
byorxyz <ohmyheroine@hotmail.com>
2017-11-14 14:47:59 -0800

    hw6 done. local run successful

--- hw6/starter-code/HW6.java
+++ hw6/starter-code/HW6.java
@@ -2,12 +2,26 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.*;
+
 import scala.Tuple2;
 
-import java.io.Serializable;
 import java.util.Comparator;
+import java.io.Serializable;
 import java.util.Collections;
+
+class DummyComparator implements Serializable, Comparator<Tuple2<String, Integer>> 
+{
+  @Override
+  public int compare(Tuple2<String, Integer> a, Tuple2<String, Integer> b) 
+    {
+    return Integer.compare(a._2(), b._2());
+    }
+}
 
 public class HW6 {
 
@@ -30,22 +44,13 @@ public class HW6 {
     Logger.getLogger("org.apache.spark.storage.BlockManager").setLevel(Level.ERROR);
 
     // use this for running locally
-    //SparkSession spark = SparkSession.builder().appName("HW6").config("spark.master", "local").getOrCreate();
+    SparkSession spark = SparkSession.builder().appName("HW6").config("spark.master", "local").getOrCreate();
 
     // use this for running on ec2
-    SparkSession spark = SparkSession.builder().appName("HW6").getOrCreate();
+    //SparkSession spark = SparkSession.builder().appName("HW6").getOrCreate();
 
-
-
-
-    /*
     Dataset<Row> r = warmup(spark, dataFile);
-    r.javaRDD().repartition(1).saveAsTextFile(output);
-    */
-
-
-
-
+    r.javaRDD().repartition(1).saveAsTextFile(output+"/warmup/");
     // uncomment each of the below to run your solutions
 
     /* Problem 1 */
@@ -55,26 +60,23 @@ public class HW6 {
     // and write to the output
     // make sure the output location does not already exists 
     // (otherwise it will throw an error)
-    r1.javaRDD().repartition(1).saveAsTextFile(output);
+    r1.javaRDD().repartition(1).saveAsTextFile(output+"/q1/");
 
     /* Problem 2 */
-    // JavaRDD<Row> r2 = Q2(spark, dataFile);
-    // r2.repartition(1).saveAsTextFile(output);
+    JavaRDD<Row> r2 = Q2(spark, dataFile);
+    r2.repartition(1).saveAsTextFile(output+"/q2/");
 
     /* Problem 3 */
-    // JavaPairRDD<Tuple2<String, Integer>, Integer> r3 = Q3(spark, dataFile);
-    // r3.repartition(1).saveAsTextFile(output);
+    JavaPairRDD<Tuple2<String, Integer>, Integer> r3 = Q3(spark, dataFile);
+    r3.repartition(1).saveAsTextFile(output+"/q3/");
 
     /* Problem 4 */
-    // Tuple2<String, Integer> r4 = Q4(spark, dataFile);
-    // spark.createDataset(Collections.singletonList(r4), Encoders.tuple(Encoders.STRING(), Encoders.INT()))
-    //      .javaRDD().saveAsTextFile(output);
+    Tuple2<String, Integer> r4 = Q4(spark, dataFile);
+    spark.createDataset(Collections.singletonList(r4), Encoders.tuple(Encoders.STRING(), Encoders.INT())).javaRDD().saveAsTextFile(output+"/q4/");
 
     /* Problem 5 */
-    // JavaPairRDD<String, Double> r5 = Q5(spark, dataFile);
-    // r5.repartition(1).saveAsTextFile(output);
-
-
+    JavaPairRDD<String, Double> r5 = Q5(spark, dataFile);
+    r5.repartition(1).saveAsTextFile(output+"/q5/");
     // this saves the results to an output file in parquet format
     // (useful if you want to generate a test dataset on an even smaller dataset)
     // r.repartition(1).write().parquet(output);
@@ -116,48 +118,73 @@ public class HW6 {
 
     Dataset<Row> df = spark.read().parquet(dataFile);
     // your code here   
-    df.createOrReplaceTempView("flights ");
+    df.createOrReplaceTempView("flights");
 
-    Dataset<Row> sqlDF = spark.sql("select DISTINCT flights, DISTINCT F.destcityname
-                                   from FLIGHTS as F
-                                   where F.origincityname = 'Seattle WA'");
+    Dataset<Row> sqlDF = spark.sql("SELECT DISTINCT destcityname FROM flights WHERE origincityname = 'Seattle, WA' ");
+
+    sqlDF.show();
+
     return sqlDF;
   }
 
   public static JavaRDD<Row> Q2 (SparkSession spark, String dataFile) {
 
-    JavaRDD<Row> d = spark.read().parquet(dataFile).javaRDD();
-
+    JavaRDD<Row> rdd = spark.read().parquet(dataFile).javaRDD();
     // your code here
-
-    return null;
+    JavaRDD<Row> result = rdd.filter(t -> {return t.getString(ORIGIN_CITY_NAME).compareTo("Seattle, WA") == 0;
+    }).map(t -> {return RowFactory.create(t.getString(DEST_CITY_NAME));}).distinct();
+    return result;
   }
+
+  //Find the number of non-cancelled flights per month that departs from each city, 
+  //return the results in a RDD where the key is a pair (i.e., a Tuple2 object),
+  //consisting of a String for the departing city name, and an Integer for the month. 
+  //The value should be the number of non-cancelled flights. 
+  //Save the EMR output as q3.txt and add it to your repo. (20 points) [Result Size: 4383 rows (281 rows on the small dataset), 17 mins on EMR]
 
   public static JavaPairRDD<Tuple2<String, Integer>, Integer> Q3 (SparkSession spark, String dataFile) {
 
-    JavaRDD<Row> d = spark.read().parquet(dataFile).javaRDD();
-
-    // your code here
-
-    return null;
+    JavaRDD<Row> df = spark.read().parquet(dataFile).javaRDD();
+    // your code here    
+    JavaPairRDD<Tuple2<String, Integer>, Integer> result = df.filter(t -> {return t.getInt(CANCELLED) == 0;}).mapToPair(t -> {return new Tuple2<Tuple2<String, Integer>, Integer>(new Tuple2<String, Integer>(t.getString(ORIGIN_CITY_NAME), t.getInt(MONTH)), 1);}).reduceByKey((a, b) -> a + b);
+    return result;
   }
+
+
+  //Find the name of the city that is connected to the most number of other cities
+  // within a single hop flight. Return the result as a pair that consists of a String
+  // for the city name, and an Integer for the total number of flights 
+  //to the other cities. Save the EMR output as q4.txt and add it to your repo. (25 points) [Result Size: 1 row, 19 mins on EMR]
 
   public static Tuple2<String, Integer> Q4 (SparkSession spark, String dataFile) {
 
     JavaRDD<Row> d = spark.read().parquet(dataFile).javaRDD();
 
     // your code here
-
-    return null;
+    d = d.map(t -> {return RowFactory.create(t.getString(ORIGIN_CITY_NAME), t.getString(DEST_CITY_NAME));}).distinct();
+    JavaPairRDD<String, Integer> tmp = d.mapToPair(t-> {return new Tuple2<String, Integer>(t.getString(1), 1);}).reduceByKey((a, b) -> a + b);
+    Tuple2<String, Integer> result = tmp.max(new DummyComparator());
+    return result;
   }
 
+
+  //Compute the average delay from all departing flights for each city. 
+  //Flights with null delay values (due to cancellation or otherwise) 
+  //should not be counted. Return the results in a RDD where 
+  //the key is a String for the city name, and the value is a Double for the average delay in minutes. Save the EMR output as q5.txt and add it to your repo. (25 points) [Result Size: 383 rows (281 rows on the small dataset), 17 mins on EMR]
+
   public static JavaPairRDD<String, Double> Q5 (SparkSession spark, String dataFile) {
-
     JavaRDD<Row> d = spark.read().parquet(dataFile).javaRDD();
-
     // your code here
-
-    return null;
+    JavaPairRDD<String, Tuple2<Integer, Integer> > tmp = d.mapToPair(
+            t -> {
+                  if (t.isNullAt(DEP_DELAY))
+                    return new Tuple2<String, Tuple2<Integer, Integer>>(t.getString(ORIGIN_CITY_NAME), new Tuple2<Integer, Integer>(0,0));
+                  else
+                    return new Tuple2<String, Tuple2<Integer, Integer>>(t.getString(ORIGIN_CITY_NAME), new Tuple2<Integer, Integer>(1, t.getInt(DEP_DELAY)));
+            }).reduceByKey((a, b) -> {return new Tuple2<Integer, Integer>(a._1 + b._1, a._2 + b._2);});
+    JavaRDD<Tuple2<String, Double>> avg_delay = tmp.map(t -> {return  new Tuple2<String, Double>(t._1, (t._2._2 * 1.0) / (t._2._1 * 1.0));});
+    return avg_delay.mapToPair(t -> t);
   }
 
   /* We list all the fields in the input data file for your reference
